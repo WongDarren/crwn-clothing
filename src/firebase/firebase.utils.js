50bd60010dd07f1aa5eb0bc43f84e14d02698bc1
WongDarren <wongdarren1997@gmail.com>
2020-06-20 21:38:36 -0700

    install firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyASwYpZrLdJ1ez415Kj2O40TkCALpS0_u4",
+  authDomain: "crwn-db-412b0.firebaseapp.com",
+  databaseURL: "https://crwn-db-412b0.firebaseio.com",
+  projectId: "crwn-db-412b0",
+  storageBucket: "crwn-db-412b0.appspot.com",
+  messagingSenderId: "543321776613",
+  appId: "1:543321776613:web:355006a95950d55cd60f0c",
+  measurementId: "G-G35WTD7HTQ"
+};
+
+firebase.initializeApp(config);
+
+// get access to .auth() from 'firebase/auth'
+// export this out to anywhere we need to use authentication
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+// gives us access to GoogleAuthProvider class from authentication library
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt:'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
