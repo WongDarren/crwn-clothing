5c3baa49486e51f71e40be3b6e7f13862c2028c0
WongDarren <wongdarren1997@gmail.com>
2020-08-26 00:53:07 -0700

    add to cart styling

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -8,9 +8,17 @@ import './custom-button.styles.scss';
 
 // pull the children off of our props that get passed into our custom button
 // spread otherProps into our custom button
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
+const CustomButton = ({
+  children, 
+  isGoogleSignIn, 
+  inverted, 
+  ...otherProps
+}) => (
   <button 
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}
+    className={`${inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'google-sign-in' : ''} 
+    custom-button`} 
+    {...otherProps}
   >
     {children}
   </button>
