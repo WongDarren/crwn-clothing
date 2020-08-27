eb73e4bfb6332d4f0094f6af2870c3c63d6e1c39
WongDarren <wongdarren1997@gmail.com>
2020-08-27 13:37:49 -0700

    separated directory and shop data, reduced directory and shop states, created collections-overview component for shops

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,79 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-// MenuItem component
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+
 import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
 
-// will need to be a CLASS component because we need to store the state value of the menu items that we need to pass and create the menu items with
-// React.Component exists in the React namespace, so we don't have to import {component} from 'react'
-class Directory extends React.Component {
-  // always need constructor() {}
-  constructor() {
-    // need to call super so that we pull in all the things we need from our React.Component into our class
-    super();
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-    // set our state value
-    // the data for sections: [] is provided in the course
-    this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          // hard coded linkUrl
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
-    };
-  }
-
-  // This is the HTML we will render.
-  render() {
-    return (
-      <div className="directory-menu">
-        {
-          // We want to map through our sections and
-          // pass the section objects into our MenuItem component so that we can create the menu items.
-          // destructure section to {id, imageUrl, title}
-          // when the keys that we pass in are the same (title = {title} imageUrl = {imageUrl}),
-          //  there is an ES6 trick we can do by spreading the values together into ...otherSectionProps
-          this.state.sections.map(({ id, ...otherSectionProps }) => (
-            // we want to map MenuItem and
-            // pass in the destructured objects
-            <MenuItem key={id} {...otherSectionProps} />
-          ))
-        }
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
 // export Directory component to use somewhere else
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,34 +1,11 @@
 import React from "react";
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    // need access to our states, so let's pass in props to super()
-    super(props);
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-    this.state = {
-      // SHOP_DATA is an array of objects that we imported
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    // destructure collections
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {
-          // map over our collections
-          // and render out CollectionPreview where key is the id
-          // so we need to destructure collections to {id, ...otherCollectionProps}
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
